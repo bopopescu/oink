b02d7cf9c5ed194932b52effe6767db1d8925484
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-10-14 17:26:49 +0530

    Users can find a vertical in the efficiency calculator now. I need to modify this so that the finder widget is an external class, so I can incorporate this into different areas of OINK. Also, I need to provide a 'use' button in each row of the search results, which emits a dictionary with that combination when used.

--- OINKModules/EfficiencyCalculator.py
+++ OINKModules/EfficiencyCalculator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 import sys
-from datetime import datetime
+import datetime
+import re
 import pandas
+
 from PyQt4 import QtGui, QtCore
 
 import MOSES
@@ -23,7 +25,6 @@ class calculatorRow:
 
     def createWidgets(self):
         """Calculator Row: Method to create widgets for a single calculator Row."""
-        #startTime = datetime.now()        
         self.calcWidgets = {}
         self.calcWidgets.update({"Description Type": QtGui.QComboBox()})
         self.calcWidgets.update({"Source": QtGui.QComboBox()})
@@ -36,21 +37,41 @@ class calculatorRow:
         self.calcWidgets.update({"Efficiency": QtGui.QLabel("00.00%")})
         self.calcWidgets.update({"Target": QtGui.QLabel("-")})
 
-        self.calcWidgets["Description Type"].setFixedWidth(180)
-        self.calcWidgets["Source"].setFixedWidth(105)
-        self.calcWidgets["BU"].setFixedWidth(150)
-        self.calcWidgets["Super-Category"].setFixedWidth(150)
-        self.calcWidgets["Category"].setFixedWidth(150)
-        self.calcWidgets["Sub-Category"].setFixedWidth(150)
-        self.calcWidgets["Vertical"].setFixedWidth(150)
+        self.calcWidgets["Description Type"].setFixedWidth(140)
+        self.calcWidgets["Source"].setFixedWidth(80)
+        self.calcWidgets["BU"].setFixedWidth(140)
+        self.calcWidgets["Super-Category"].setFixedWidth(140)
+        self.calcWidgets["Category"].setFixedWidth(140)
+        self.calcWidgets["Sub-Category"].setFixedWidth(140)
+        self.calcWidgets["Vertical"].setFixedWidth(140)
         self.calcWidgets["Quantity"].setFixedWidth(60)
         self.calcWidgets["Efficiency"].setFixedWidth(60)
         self.calcWidgets["Target"].setFixedWidth(60)
 
-        #functionName = sys._getframe().f_code.co_name
-        #endTime = datetime.now()
-        #timeSpent = endTime - startTime
-        #print "Spent %s in %s." % (timeSpent, functionName)
+        self.calcWidgets["Description Type"].setFixedHeight(20)
+        self.calcWidgets["Source"].setFixedHeight(20)
+        self.calcWidgets["BU"].setFixedHeight(20)
+        self.calcWidgets["Super-Category"].setFixedHeight(20)
+        self.calcWidgets["Category"].setFixedHeight(20)
+        self.calcWidgets["Sub-Category"].setFixedHeight(20)
+        self.calcWidgets["Vertical"].setFixedHeight(20)
+        self.calcWidgets["Quantity"].setFixedHeight(20)
+        self.calcWidgets["Efficiency"].setFixedHeight(20)
+        self.calcWidgets["Target"].setFixedHeight(20)
+
+
+        style_sheet = """
+                .QLabel, .QComboBox, .QSpinBox {
+                    font: 11px;
+                }
+        """
+        for widget_label in self.calcWidgets.keys():
+            self.calcWidgets[widget_label].setStyleSheet(style_sheet)
+
+
+    def alertMessage(self, title, message):
+        """Vindaloo."""
+        QtGui.QMessageBox.about(self, title, message)
 
     def mapToolTips(self):
         """Calculator Row"""
@@ -79,17 +100,13 @@ class calculatorRow:
 
     def updateEfficiency(self):
         """Calculator Row"""
-        #startTime = datetime.now()
+
         efficiency = self.getEfficiency()
         if efficiency is None:
             efficiency_text = "No Target"
         else:
             efficiency_text = "%.2f%%" % (efficiency)
         self.calcWidgets["Efficiency"].setText(efficiency_text)
-        functionName = sys._getframe().f_code.co_name
-        #endTime = datetime.now()
-        #timeSpent = endTime - startTime
-        #print "Spent %s in %s." % (timeSpent, functionName)
    
     def populateTypeSource(self):
         """Calculator Row"""
@@ -163,7 +180,7 @@ class calculatorRow:
             "Sub-Category": str(self.calcWidgets["Sub-Category"].currentText()),
             "Vertical": str(self.calcWidgets["Vertical"].currentText())
         }
-        request_date = datetime.date(startTime)
+        request_date = datetime.date.today()
 
         target = float(MOSES.getTargetFor(self.userID, self.password, row, request_date))
 
@@ -192,11 +209,11 @@ class EfficiencyCalculator(QtGui.QDialog):
 
     def createWidgets(self):
         """Efficiency Calculator: Creates widgets."""
-        #startTime = datetime.now()
-        #functionName = sys._getframe().f_code.co_name
         self.calc_table = QtGui.QTableWidget()
-        self.headers = ["Description Type","Source","BU","Super-Category","Category","Sub-Category","Vertical","Quantity","Efficiency","Target"]
+        self.category_tree_headers = ["BU","Super-Category","Category","Sub-Category","Vertical"]
+        self.headers = ["Description Type","Source"] + self.category_tree_headers + ["Quantity","Efficiency","Target"]
         self.calc_table.setColumnCount(len(self.headers))
+        self.calc_table.setMinimumWidth(1200)
         self.addCalcRow()
 
         
@@ -205,33 +222,96 @@ class EfficiencyCalculator(QtGui.QDialog):
         self.refreshButton = QtGui.QPushButton("Recalculate Efficiency")
         self.totEffLabel = QtGui.QLabel("<font size=3 face=Georgia><b>Total Efficiency:</b></font>")
         self.effScoreLabel = QtGui.QLabel("<font size=3 face=Georgia><b>00.00%</b></font>")
-        #endTime = datetime.now()
-        #timeSpent = endTime - startTime
-        #print "Spent %s in %s." % (timeSpent, functionName)
+        
+        #Finder
+        self.finder_label = QtGui.QLabel("Search Based On:")
+        
+        self.search_criteria_combo_box = QtGui.QComboBox()
+        self.search_criteria_combo_box.addItems(self.category_tree_headers+["Any"])
+        
+        self.search_string_line_edit = QtGui.QLineEdit()
+
+        self.find_button = QtGui.QPushButton("Find")
+
+        self.result_table = QtGui.QTableWidget()
+        self.result_table.setRowCount(0)
+        self.result_table.setColumnCount(len(self.category_tree_headers))
+        self.result_table.setHorizontalHeaderLabels(self.category_tree_headers)
+        self.result_table.resizeColumnsToContents()
+
+        self.find_widget_layout = QtGui.QGridLayout()
+
+        self.find_widget_layout.addWidget(self.finder_label,0,0 )
+        self.find_widget_layout.addWidget(self.search_criteria_combo_box,0, 1)
+        self.find_widget_layout.addWidget(self.search_string_line_edit,0, 2, 1, 2)
+        self.find_widget_layout.addWidget(self.find_button,0,4,1,1)
+        self.find_widget_layout.addWidget(self.result_table,1,0,2,5)
+
+        self.find_group = QtGui.QGroupBox("Find a Vertical")
+        self.find_group.setLayout(self.find_widget_layout)
 
     def createLayouts(self):
         """Efficiency Calculator."""
-        #startTime = datetime.now()
-        #functionName = sys._getframe().f_code.co_name
-        
         self.effLayout = QtGui.QHBoxLayout()
         self.effLayout.addWidget(self.plusButton)
         self.effLayout.addWidget(self.refreshButton)
         self.effLayout.addStretch(2)
         self.effLayout.addWidget(self.totEffLabel)
         self.effLayout.addWidget(self.effScoreLabel)
-        
-        self.finalLayout = QtGui.QVBoxLayout()
 
-        self.finalLayout.addWidget(self.calc_table,3)
-        self.finalLayout.addStretch(1)
-        self.finalLayout.addLayout(self.effLayout)
+        self.finalLayout = QtGui.QVBoxLayout()
+        self.finalLayout.addWidget(self.calc_table,1)
+        self.finalLayout.addWidget(self.find_group,1)
+        self.finalLayout.addLayout(self.effLayout,0)
 
         self.setLayout(self.finalLayout)
 
-        #endTime = datetime.now()
-        #timeSpent = endTime - startTime
-        #print "Spent %s in %s." % (timeSpent, functionName)
+    def findIdentifierInCategoryTree(self, search_string, search_criteria):        
+        dfs = [self.category_tree[self.category_tree[search_criteria].str.contains(search_string)]]
+        dfs.append(self.category_tree[self.category_tree[search_criteria].str.contains(search_string.lower())])
+        dfs.append(self.category_tree[self.category_tree[search_criteria].str.contains(search_string.capitalize())])
+        dfs.append(self.category_tree[self.category_tree[search_criteria].str.contains(search_string.upper())])
+        return pandas.concat(dfs)
+
+    def findIdentifier(self):
+        """Efficiency Calculator."""
+        #functionName = sys._getframe().f_code.co_name
+        search_string = str(self.search_string_line_edit.text()).strip()
+        search_criteria = str(self.search_criteria_combo_box.currentText())
+        df = None
+        if search_criteria != "Any":
+            if search_string != "":
+                df = self.findIdentifierInCategoryTree(search_string, search_criteria)
+                self.category_tree[self.category_tree[search_criteria].str.contains(search_string)]
+                df.drop_duplicates(subset=self.category_tree_headers, inplace=True)
+                self.alertMessage("Alert", "%d results found when searching for %s in the %s column of the category tree."%(len(df.index),search_string, search_criteria))
+            else:
+                self.alertMessage("Alert","Try typing a word into the search field before searching for something.")
+        else:   
+            dfs = []     
+            for search_criteria in self.category_tree_headers:
+                dfs.append(self.findIdentifierInCategoryTree(search_string, search_criteria))
+            df = pandas.concat(dfs)
+            df.drop_duplicates(subset=self.category_tree_headers, inplace=True)
+            self.alertMessage("Alert", "%d results found when searching for %s in all columns of the category tree."%(len(df.index),search_string))
+
+        if df is not None:
+            self.result_table.setRowCount(0)
+            row_counter = 0
+            for row in df.iterrows():
+                #print row[1]
+                self.result_table.insertRow(row_counter)
+                column_counter = 0
+                for column_name in self.category_tree_headers:
+                    string_value = str(row[1][column_name])
+                    self.result_table.setItem(row_counter, column_counter, QtGui.QTableWidgetItem(string_value))
+                    column_counter += 1
+                row_counter += 1
+
+            self.result_table.setHorizontalHeaderLabels(self.category_tree_headers)
+            self.result_table.resizeColumnsToContents()
+            self.result_table.resizeRowsToContents()
+
 
     def addCalcRow(self):
         """Efficiency Calculator."""
@@ -239,7 +319,7 @@ class EfficiencyCalculator(QtGui.QDialog):
         self.calcList.append(calculatorRow(self.userID, self.password, self.category_tree)) 
         #print "Printing Calc List", self.calcList
         row_count = int(self.calc_table.rowCount())
-        self.calc_table.insertRow(row_count)
+        self.calc_table.insertRow(0)
         row = row_count
         column = 0
 
@@ -247,30 +327,25 @@ class EfficiencyCalculator(QtGui.QDialog):
             self.calc_table.setCellWidget(row, column, self.calcList[-1].calcWidgets[widget_name])
             column+=1
         self.calc_table.setHorizontalHeaderLabels(self.headers)
+        self.calc_table.resizeRowsToContents()
         self.calc_table.resizeColumnsToContents()
 
     
     def createEvents(self):
         """Efficiency Calculator."""
-        #startTime = datetime.now()
         self.plusButton.clicked.connect(self.plusAction)
         self.refreshButton.clicked.connect(self.displayEfficiency)
         self.connectQuantityWidgets()
-        functionName = sys._getframe().f_code.co_name
-        #endTime = datetime.now()
-        #timeSpent = endTime - startTime
-        #print "Spent %s in %s." % (timeSpent, functionName)
+        self.find_button.clicked.connect(self.findIdentifier)        
+
 
     def setVisuals(self):
         """Efficiency Calculator."""
-        #startTime = datetime.now()
         functionName = sys._getframe().f_code.co_name
         self.setWindowTitle("Efficiency Calculator: Driving What Drives You")
         self.setWindowIcon(QtGui.QIcon('Images\PORK_Icon.png'))
         self.show()
-        #endTime = datetime.now()
-        #timeSpent = endTime - startTime
-        #print "Spent %s in %s." % (timeSpent, functionName)
+
 
     def plusAction(self):
         """Efficiency Calculator."""
@@ -284,25 +359,16 @@ class EfficiencyCalculator(QtGui.QDialog):
         
     def displayEfficiency(self):
         """Efficiency Calculator."""
-        #startTime = datetime.now()
         totalEfficiency = 0.0
         for calc_row in self.calcList:
             calc_row.updateEfficiency()
             efficiency = calc_row.efficiency if calc_row.efficiency is not None else 0
             totalEfficiency += efficiency
         self.effScoreLabel.setText("%.2f%%"%totalEfficiency)
-        functionName = sys._getframe().f_code.co_name
-        #endTime = datetime.now()
-        #timeSpent = endTime - startTime
-        #print "Spent %s in %s." % (timeSpent, functionName)
 
-    def findIdentifier(self):
-        """Efficiency Calculator."""
-        startTime = datetime.now()
-        functionName = sys._getframe().f_code.co_name
-        endTime = datetime.now()
-        timeSpent = endTime - startTime
-        print "Spent %s in %s." % (timeSpent, functionName)
+    def alertMessage(self, title, message):
+        """Vindaloo."""
+        QtGui.QMessageBox.about(self, title, message)
 
 if __name__ == "__main__":
     import sys
