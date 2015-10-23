c419f28d0edcd65e3e0b2e00ad7873ddada0e4af
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-10-23 15:30:02 +0530

    Adding icons to WeekCalendar. I can draw pixmaps on any date now. Works like a charm.

--- OINKModules/WeekCalendar.py
+++ OINKModules/WeekCalendar.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-from PyQt4 import QtGui, QtCore
-
+import os
 import datetime
+
+from PyQt4 import QtGui, QtCore
 from Porker import Porker
 
 import MOSES
@@ -40,32 +41,6 @@ class WeekCalendar(QtGui.QCalendarWidget):
         8. A horizontal bar is painted at the bottom of each working day. The bar is red till 25%, 
             orange from 25% to 75%, a mix of both from 75% to 99.99% and green at and above 100%. 
         """
-        #start inner function
-        def paintCellThreaded(efficiency, cfm, gseo):
-            gradient = QtGui.QLinearGradient(progress_origin, progress_end)
-            gradient.setSpread(QtGui.QGradient.PadSpread)
-            sub_rect = QtCore.QRect(sub_rect_topleft, sub_rect_bottomright)
-            if efficiency <= 0.25:
-                progress_bar_color = progress_bar_00_color
-            elif 0.25 < efficiency <= 0.5:
-                progress_bar_color = progress_bar_25_color
-            elif 0.5 < efficiency <= 0.75:
-                progress_bar_color = progress_bar_50_color
-            elif 0.75< efficiency < 0.99: 
-            #Fix this. The efficiency should not return 99.99999% even when the writers achieve target. 
-                progress_bar_color = progress_bar_75_color
-            elif 0.99 < efficiency <= 1.0:
-                progress_bar_color = progress_bar_100_color
-            else:
-                progress_bar_color = progress_bar_101_color
-            if efficiency > 1.0:
-                efficiency = 1.0
-            steps = [float(x)/100.00 for x in range(int(efficiency*100))]
-            for x in steps:
-                gradient.setColorAt(x, progress_bar_color)
-            gradient.setColorAt(efficiency, white_color)
-            gradient.setColorAt(1, white_color)
-            painter.fillRect(sub_rect, gradient)
         self.counter +=1
         #if self.counter % 30 == 0:
         #    print "Repainting cells in weekcalendar. Counter: ", self.counter
@@ -82,7 +57,7 @@ class WeekCalendar(QtGui.QCalendarWidget):
         disabled_color = QtGui.QColor(192, 192, 192) #Greyed
         background_all_color = QtGui.QColor(192, 192, 192) #Greyed
         white_color = QtGui.QColor(255, 255, 255) #White
-        selection_color = QtGui.QColor(0, 255, 255) #cyan
+        selection_color = QtGui.QColor(174, 229, 129) #light green
         leave_color = QtGui.QColor(96, 96, 96)
         holiday_color = QtGui.QColor(59,59,59) 
         #Coordinates of the rectangle.
@@ -114,18 +89,47 @@ class WeekCalendar(QtGui.QCalendarWidget):
                 cfm = None
                 gseo = None
             #Start painting the cell.
-            if date == datetime.date.today():
-                cell_color = today_color
-            elif date == self.selectedDate():
-                cell_color = selection_color
-            elif status == "Working":
-                cell_color = white_color
+            if status == "Working":
+                if (date == datetime.date.today()):
+                    cell_color = selection_color
+                else:
+                    cell_color = white_color
             elif status == "Leave" or status == "Holiday":
                 cell_color = holiday_color
             else:
                 cell_color = disabled_color
-
             painter.fillRect(rect, cell_color)
+            if date == self.selectedDate():
+                image_path = os.path.join("Images","circle_today.png")
+                type = os.path.splitext(os.path.basename(str(image_path)))[1]
+                selection_marker_pixmap = QtGui.QPixmap(image_path, type)
+                width, height = 20, 20
+                selection_marker_pixmap = selection_marker_pixmap.scaled(
+                                                        QtCore.QSize(width, height),
+                                                        QtCore.Qt.KeepAspectRatio, 
+                                                        QtCore.Qt.SmoothTransformation
+                                                        )
+                x = int(x_left+x_range*0.4)
+                x = center_coords.x()-selection_marker_pixmap.width()/2
+                y = int(y_bottom+y_range*0.5)
+                y = center_coords.y()-selection_marker_pixmap.height()/2
+                painter.drawPixmap(x, y, selection_marker_pixmap)
+            if status == "Working":
+                if relaxation is not None:
+                    if relaxation >0:
+                        image_path = os.path.join("Images","download.png")
+                        type = os.path.splitext(os.path.basename(str(image_path)))[1]
+                        relaxation_pixmap = QtGui.QPixmap(image_path, type)
+                        width, height = 10, 10
+                        relaxation_pixmap = relaxation_pixmap.scaled(
+                                                                QtCore.QSize(width, height),
+                                                                QtCore.Qt.KeepAspectRatio, 
+                                                                QtCore.Qt.SmoothTransformation
+                                                                )
+                        x = int(x_right-width*1.1)
+                        y = int(y_top+height*0.1)
+                        painter.drawPixmap(x, y, relaxation_pixmap)
+                #print "Drew relaxation pixmap"
             #if status == "Working":
 
             #Fill the efficiency bar
@@ -221,5 +225,11 @@ class WeekCalendar(QtGui.QCalendarWidget):
                 gseo_marker_rect = QtCore.QRect(gseo_marker_topleft, gseo_marker_bottomright)
                 painter.fillRect(gseo_marker_rect, gseo_gradient)
         #Finally, paint the date over the cell.
-        painter.drawText(QtCore.QPoint((x_left+x_range*0.4), (y_bottom + y_range*0.5)), "%s" %date.toPyDate().day)
+        day_number = date.toPyDate().day
+        if day_number > 9:
+            x = (x_left+x_range*0.4)
+        else:
+            x = (x_left+x_range*0.5)
+        y = center_coords.y()+5
+        painter.drawText(QtCore.QPoint(x, y), "%s" %date.toPyDate().day)
         
