b398c8440469401dc8e2a96830f3c14c861aa600
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-11-12 11:30:49 +0530

    Added the SharinganButton.

--- OINKModules/ImageButton.py
+++ OINKModules/ImageButton.py
@@ -1,7 +1,12 @@
 from __future__ import division
 import os
+import time
+import datetime
+import sys
 from PyQt4 import QtGui, QtCore
-
+from PIL import ImageQt
+from PIL import Image
+import PIL
 
 class ImageButton(QtGui.QPushButton):
     def __init__(self, image_path, height=None, width=None, mouseover_image_path=None):
@@ -28,8 +33,8 @@ class ImageButton(QtGui.QPushButton):
         self.showImage(self.image_path)
 
     def showImage(self, image_path):
-        type = os.path.splitext(os.path.basename(str(image_path)))[1]
-        image_pixmap = QtGui.QPixmap(image_path, type)
+        type_ = os.path.splitext(os.path.basename(str(image_path)))[1]
+        image_pixmap = QtGui.QPixmap(image_path, type_)
 
         image_pixmap = image_pixmap.scaled(
                                         QtCore.QSize(self.width, self.height),
@@ -48,4 +53,16 @@ class ImageButton(QtGui.QPushButton):
         self.showImage(self.image_path)
 
         super(ImageButton, self).leaveEvent(evnt)
-                
+    
+    def updateImage(self, qimage_object):
+        type_ = os.path.splitext(os.path.basename(str(self.image_path)))[1]
+        image_pixmap = QtGui.QPixmap(self.image_path, type_)
+        conversion_status = image_pixmap.convertFromImage(qimage_object)
+        image_pixmap = image_pixmap.scaled(
+                                        QtCore.QSize(self.width, self.height),
+                                        QtCore.Qt.KeepAspectRatio, 
+                                        QtCore.Qt.SmoothTransformation)
+        icon = QtGui.QIcon(image_pixmap)
+        self.setIcon(icon)
+        self.setIconSize(image_pixmap.rect().size())
+        self.setFixedSize(image_pixmap.rect().size())
--- OINKModules/SharinganButton.py
+++ OINKModules/SharinganButton.py
@@ -7,17 +7,61 @@ import glob
 import datetime
 
 from PyQt4 import QtGui, QtCore
+from PIL import Image, ImageQt
+import PIL
+
 from ImageButton import ImageButton
+
+class ThreadOfSixPaths(QtCore.QThread):
+    updateImage = QtCore.pyqtSignal(ImageQt.ImageQt)
+    def __init__(self):
+        super(ThreadOfSixPaths, self).__init__()
+        self.mutex = QtCore.QMutex()
+        self.condition = QtCore.QWaitCondition()
+        self.delay = 5
+        self.rotation_delay = 0.05
+        self.active = True
+        self.sharingan_file_lists = getSharinganFileLists()
+        if not self.isRunning():
+            self.start(QtCore.QThread.LowPriority)
+
+    def run(self):
+        while True:
+            if self.active:
+                for sharingan_list in self.sharingan_file_lists:
+                    self.useImages(sharingan_list)
+                    time.sleep(self.delay)
+                    self.useImages(list(reversed(sharingan_list)))
+                    time.sleep(self.delay)
+
+
+    def useImages(self, sharingan_images_list):
+        sweep = 360
+        frames_per_delay = 10
+        image_rotation_delay = self.rotation_delay
+        forward_sweep_angle_steps = range(0, sweep, frames_per_delay)
+        reversed_sweep_steps = list(reversed(forward_sweep_angle_steps))
+        reversed_sweep_steps.extend(forward_sweep_angle_steps) #Pillow will rotate counterclockwise.
+        sweep_steps = reversed_sweep_steps
+        for sharingan_stage_image in sharingan_images_list:
+            for i in sweep_steps:
+                self.updateImage.emit(self.rotateImage(sharingan_stage_image, i))
+                time.sleep(image_rotation_delay)
+
+    def rotateImage(self, sharingan_stage_image, angle):
+        image_object = Image.open(sharingan_stage_image).convert("RGBA").rotate(angle)
+        return ImageQt.ImageQt(image_object)
+
+
 
 class SharinganButton(ImageButton):
     def __init__(self, *args, **kwargs):
         super(SharinganButton, self).__init__(*args, **kwargs)
-        self.createUI()
-        self.mapEvents()
-    def createUI(self):
-        self.show()
-    def mapEvents(self):
-        pass
+        self.ashura = ThreadOfSixPaths()
+        self.ashura.updateImage.connect(self.updateImage)
+
+    def updateImage(self, image_object):
+        super(SharinganButton, self).updateImage(image_object)
 
 def getSharinganFileLists():
     import os
@@ -38,10 +82,16 @@ def getSharinganFileLists():
             eternal_mangyeko_file_list = sorted(glob.glob(eternal_mangyeko_search_string))
             mangyeko_pair.extend(eternal_mangyeko_file_list)
             used_files.extend(eternal_mangyeko_file_list)
-            file_pairs.append([os.path.basename(x) for x in mangyeko_pair])
+            file_pairs.append([x for x in mangyeko_pair])
 
     return file_pairs
 
 if __name__ == "__main__":
-    print getSharinganFileLists()
+    app = QtGui.QApplication(sys.argv)
+    sharingan_button = SharinganButton(os.path.join("..","Images","sharingan","uchiha_eye"),100,100)
+    test_window = QtGui.QMainWindow()
+    test_window.setCentralWidget(sharingan_button)
+    test_window.show()
+    sys.exit(app.exec_())
+
 
